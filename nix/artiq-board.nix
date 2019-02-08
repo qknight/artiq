eb7a6019838b1b7c3fc3517fffcbb01725be177c
Joachim Schiele <js@lastlog.de>
2019-02-08 20:15:12 +0100

    using fetchcargo now and transformin the input

--- nix/artiq-board.nix
+++ nix/artiq-board.nix
@@ -8,22 +8,68 @@
 
 let 
   pkgs = import <nixpkgs> {};
+  fetchcargo = import <nixpkgs/pkgs/build-support/rust/fetchcargo.nix> {
+    inherit (pkgs) stdenv cacert git rust cargo-vendor;
+  };
+  myVendoredSrc1 = fetchcargo rec {
+    name = "myVendoredSrc1";
+    sourceRoot = null;
+    srcs = null;
+    src = ../artiq/firmware;
+    cargoUpdateHook = "";
+    patches = [];
+    sha256 = "1jsb5f5m8p00ikw231sl0i6jg2ilpc2wv1yzi47dgpb54z3gjx3g";
+  };
+
+  myVendoredSrc2 = pkgs.stdenv.mkDerivation {
+    name = "myVendoredSrc2";
+    src = myVendoredSrc1;
+    phases = [ "unpackPhase" "installPhase" ];
+    installPhase = ''
+      mkdir -p $out/.cargo/registry
+      cat > $out/.cargo/config <<-EOF
+        [source.crates-io]
+        registry = 'https://github.com/rust-lang/crates.io-index'
+        replace-with = 'vendored-sources'
+
+        [source.vendored-sources]
+        directory = '$out/.cargo/registry'
+      EOF
+      cat $out/.cargo/config
+
+      cp -R * $out/.cargo/registry
+    '';
+  };
+
+  # qknight: code below made the artiq targets (firmware) build!
+  #          however, it is hardcoded and needs to be implemented by fetchcargo instead
+  #myVendoredSrc = pkgs.stdenv.mkDerivation {
+  #  name = "myVendoredSrc";
+  #  src = /home/joachim/cargo;
+  #  phases = [ "unpackPhase" "installPhase" ];
+  #  installPhase = ''
+  #    mkdir -p $out/.cargo
+  #    cp -R registry $out/.cargo
+  #  '';
+  #};
+
   buildenv = import ./artiq-dev.nix {
     extraProfile = ''
-       export HOME=`pwd`
+      export HOME=${myVendoredSrc2}
     '';
     # --no-compile-gateware to disable vivado build
     runScript = "python -m artiq.gateware.targets.kasli -V satellite --no-compile-gateware";
   };
+#in myVendoredSrc2
 in pkgs.stdenv.mkDerivation {
   name = "artiq-board";
   src = null;
-  buildInputs = [ strace ];
+  #buildInputs = [ strace ];
   phases = [ "buildPhase" "installPhase" ];
   buildPhase = buildenv + "/bin/artiq-dev";
+    #cp artiq_kasli/satellite/gateware/top.bit $out
   installPhase = ''
     mkdir $out
-    cp artiq_kasli/satellite/gateware/top.bit $out
     cp artiq_kasli/satellite/software/bootloader/bootloader.bin $out
     cp artiq_kasli/satellite/software/satman/satman.{elf,fbi} $out
   '';
--- nix/firmware.nix
+++ nix/firmware.nix
@@ -1,36 +1,59 @@
 # nix-build -I nixpkgs=./nixpkgs --option sandbox false -A firmware
 let
   artiq-dev = import ./artiq-dev.nix {};
+  artiqpkgs = import ./default.nix { inherit pkgs; };
   pkgs = import <nixpkgs> {};
   buildenv = import ./artiq-dev.nix {
-    extraProfile = ''
-       export HOME=`pwd`
-    '';
+    #extraProfile = ''
+    #  export HOME=`pwd`
+    #'';
     runScript = "python -m artiq.gateware.targets.kasli -V satellite --no-compile-gateware";
+  };
+  myVendoredSrc = pkgs.stdenv.mkDerivation {
+    name = "myVendoredSrc";
+    src = /home/joachim/cargo;
+    phases = [ "unpackPhase" "installPhase" ];
+    #buildInputs = with artiqpkgs; [ cargo rustc ];
+    #fetchPhase = ''
+    #  echo "hi"
+    #  cd $src
+    #  mkdir $src/foo
+    #  ls $src
+    #  export HOME=$src
+    #  echo "pre-populate the cargo cache, see https://users.rust-lang.org/t/pre-caching-cargo-index-and-crates/18561"
+    #  cargo search whatever
+    #'';
+    installPhase = ''
+      mkdir -p $out/.cargo
+      cp -R registry $out/.cargo
+    '';
   };
 in
 { stdenv, fetchFromGitHub, fetchurl, runCommand, rustPlatform }:
   
 rustPlatform.buildRustPackage rec {
   name = "firmware";
-  buildPhase = ''
-    export HOME=`pwd`
-  '';
+  #fetchPhase = ''
+  #  export HOME=`pwd`
+  #'';
+  #buildPhase = ''
+  #  echo "tut23"
+  #  exit 1
+  #'';
 
   src =
     let
       source = ../artiq/firmware;
     in
+      #du -a ${myVendoredSrc}
     runCommand "cargo-firmware-src" {} ''
-      echo "hi 1"
+      export HOME=${myVendoredSrc}
       cp -R ${source} $out
-      echo "hi 2"
       chmod +w $out
-      echo "hi 3"
       ${buildenv + "/bin/artiq-dev"}
     '';
 
-  cargoSha256 = "1xzjn9i4rkd9124v2gbdplsgsvp1hlx7czdgc58n316vsnrkbr86";
+  cargoSha256 = "1jsb5f5m8p00ikw231sl0i6jg2ilpc2wv1yzi47dgpb54z3gjx3g";
 
   meta = with stdenv.lib; {
     description = "asdf";
